0fb7da47ae92a87e89b6a0814def49995e264e34
marlonx19 <marlon.englemam@gmail.com>
2020-06-01 16:23:18 -0300

    added feature to emit message from user to driver

--- src/server.js
+++ src/server.js
@@ -5,13 +5,41 @@ const path = require('path');
 
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, 'files')));
 app.use(routes);
 
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-app.listen(process.env.PORT || 3000, function () {
+// Namespaces
+const usuarios = io.of('/usuarios');
+const motoristas = io.of('/motoristas');
+
+
+usuarios.on('connection', (socket) => {
+  console.log('a user connected to usuarios');
+
+  socket.on('continue', () => {
+    motoristas.emit('hello')
+  })
+
+  usuarios.emit('connected', socket.client.conn.server.clientsCount)
+
+});
+
+
+motoristas.on('connection', (socket) => {
+  console.log('a user connected to motoristas');
+  const newNamespace = socket.nsp;
+
+  newNamespace.emit('connected', socket.client.conn.server.clientsCount)
+  //io.emit('connected', socket.client.conn.server.clientsCount)
+});
+
+
+
+http.listen(process.env.PORT || 3000, function () {
   console.log('Backend Moov is listening!');
 });
