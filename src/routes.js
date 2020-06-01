6943b18eea3121c3921e5c1015b9666c6a5de2ad
marlonx19 <marlon.englemam@gmail.com>
2020-06-01 00:44:49 -0300

    updated controllers names

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,8 @@ const express = require('express');
 const multer = require('multer');
 const uploadConfig = require('./config/upload');
 const UserController = require('./controllers/UserController');
-const DoctorController = require('./controllers/DoctorController');
-const ConsultationController = require('./controllers/ConsultationController');
-const SpecializationController = require('./controllers/SpecializationController');
-const DocSpec = require('./controllers/DoctorSpecializationController');
+const DriverController = require('./controllers/DriverController');
+const DeliveryController = require('./controllers/DeliveryController');
 const AuthController = require('./controllers/AuthController');
 
 const routes = express.Router();
@@ -16,25 +14,15 @@ routes.get('/users', UserController.listAll);
 routes.get('/user', UserController.listData);
 routes.post('/users', uploads.single('userphoto'), UserController.store);
 
-routes.get('/doctors', DoctorController.listAll);
-routes.get('/doctor', DoctorController.listData);
-routes.post('/doctors', uploads.single('doctorphoto'), DoctorController.store);
+
+routes.get('/drivers', DriverController.listAll);
+routes.get('/driver', DriverController.listData);
+routes.post('/drivers', uploads.single('driverphoto'), DriverController.store);
 
 
-routes.post('/consultations', ConsultationController.store);
-routes.get('/consultations', ConsultationController.listAll);
-routes.get('/consult', ConsultationController.listConsult);
-
-routes.post('/specializations', SpecializationController.store);
-routes.get('/specializations', SpecializationController.listAll);
-// routes.get('/consultations', ConsultationController.listAll);
-// routes.get('/consult', ConsultationController.listConsult);
-
-routes.post('/docespec', DocSpec.store);
-routes.get('/docespec', DocSpec.listDoctors);
-routes.get('/alldocespec', DocSpec.listAllDoctors);
-// routes.get('/consultations', ConsultationController.listAll);
-// routes.get('/consult', ConsultationController.listConsult);
+routes.post('/deliveries', DeliveryController.store);
+routes.get('/deliveries', DeliveryController.listAll);
+routes.get('/delivery', DeliveryController.listConsult);
 
 
 routes.post('/login', AuthController.Login);
